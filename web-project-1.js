2fa3587573709b85c4cd920ef5df5873393f9004
farhazizah <139628400+farhazizah@users.noreply.github.com>
2023-10-03 07:21:28 +0700

    Update web-project-1.js
    dark mode

--- web-project-1.js
+++ web-project-1.js
@@ -6,5 +6,86 @@ menu.addEventListener("click", function () {
 })
 
 window.onscroll = () => {
-    navbar.classList.remove("active")
+  navbar.classList.remove("active")
 };
+
+//dark theme
+const icon = document.getElementById('sun');
+const body = document.querySelector('body');
+const p = document.getElementsByTagName('p');
+const h2 = document.getElementsByTagName('h2');
+//tag dalam menu 
+const h2Menu = document.querySelectorAll('.box h2');
+const pMenu = document.querySelectorAll('.box p');
+//tag dalam contact
+const h2Contact =document.querySelectorAll('.contact h2');
+//tag dalam footer
+const spanFooter = document.querySelectorAll('.col span');
+const liFooter = document.querySelectorAll('.col ul li a');
+const iconFooter = document.querySelectorAll('.social i:hover');
+
+
+icon.addEventListener('click', function() {
+  this.classList.toggle('bxs-moon');
+   if(this.classList.toggle('bxs-sun')) {
+        body.style.background = '#150129';
+        body.style.transition = '1s';
+        this.innerHTML = 'Bright Mode';
+        //tag p
+        for (let i = 0; i < p.length; i++) {
+            p[i].style.color = 'white';
+        }
+        //tag h2
+        for (let i = 0; i < h2.length; i++) {
+            h2[i].style.color = 'white';
+        }
+        //tag h2 menu
+        for (let i = 0; i < h2Menu.length; i++) {
+            h2Menu[i].style.color = '#150129';
+        }
+        //tag p menu
+        for (let i = 0; i < pMenu.length; i++) {
+            pMenu[i].style.color = '#150129';
+        }
+        //tag h2 contact
+        for (let i = 0; i < h2Contact.length; i++) {
+            h2Contact[i].style.color = '#150129';
+        }
+        //tag dalam footer
+        for (let i = 0; i < spanFooter.length; i++) {
+            spanFooter[i].style.color = 'white';
+        }
+        for (let i = 0; i < liFooter.length; i++) {
+            liFooter[i].style.color = 'white';
+        }
+        for (let i = 0; i < iconFooter.length; i++) {
+            iconFooter[i].style.background = '#de6717';
+            }
+
+    } else{
+        body.style.background = 'white';
+        this.innerHTML = 'Dark Mode';
+        
+        //tag p
+        for (let i = 0; i < p.length; i++) {
+            p[i].style.color = '#150129';
+        }
+        //tag h2
+        for (let i = 0; i < h2.length; i++) {
+            h2[i].style.color = '#150129';
+        }
+       //tag dalam footer
+        for (let i = 0; i < spanFooter.length; i++) {
+        spanFooter[i].style.color = '#150129';
+        }
+        for (let i = 0; i < liFooter.length; i++) {
+            liFooter[i].style.color = '#150129';
+            liFooter[i].style.fontWeight= '200';
+        }
+        for (let i = 0; i < iconFooter.length; i++) {
+            iconFooter[i].onmouseover= '#150129';
+            }
+        body.style.transition = '1s';
+    }
+
+})
